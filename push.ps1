efa8f1dad17e104ce116b551d4d3502975dc07db
huhuiyu <1069306849@qq.com>
2022-06-16 20:59:02 +0800

    添加远程库的指令

--- push.ps1
+++ push.ps1
@@ -1,4 +1,4 @@
-# github链接地址：git@github.com:huhuiyu/document.git
+# github链接地址：git remote add github git@github.com:huhuiyu/document.git
 # 阿里云链接地址：git@code.aliyun.com:DarkKnight/document.git
 git push
 git push github
